76ebafb5531859eff8025c4da420d0d9b6418480
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2024-08-22 21:28:39 +0200

    update style of plots

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,8 +10,9 @@ from tqdm.notebook import tqdm
 import copy
 from pprint import pprint
 from sympy.ntheory.modular import solve_congruence
-import gurobipy as gp
-from gurobipy import GRB
+
+# import gurobipy as gp
+# from gurobipy import GRB
 from copy import deepcopy
 import pickle as pck
 from functools import cache
@@ -19,6 +20,7 @@ from sympy import totient, isprime, divisors
 from sympy.ntheory.residue_ntheory import mobius
 import networkx as nx
 from fractions import Fraction
+
 
 def de_bruijn_invBWT(n, k):
     """de Bruijn sequence for alphabet k
@@ -50,43 +52,54 @@ def de_bruijn_invBWT(n, k):
     db(1, 1)
     return "".join(alphabet[i] for i in sequence)
 
+
 def create_dbg(n, sigma):
     G = nx.DiGraph()
-    nodes = ["".join(x) for x in (itertools.product([str(i) for i in range(sigma)], repeat=n))]
+    nodes = [
+        "".join(x)
+        for x in (itertools.product([str(i) for i in range(sigma)], repeat=n))
+    ]
     for n in nodes:
         for c in range(sigma):
             G.add_edge(n, n[1:] + str(c))
-    
+
     return G
 
+
 def necklace(n, sigma=2):
-    return int((sum(totient(d)*(sigma**(n // d)) for d in divisors(n))) // n)
+    return int((sum(totient(d) * (sigma ** (n // d)) for d in divisors(n))) // n)
+
 
 def aperiodic_necklaces(n, sigma=2):
-    return int((sum(mobius(d) * sigma**(n // d) for d in divisors(n))) // n)
+    return int((sum(mobius(d) * sigma ** (n // d) for d in divisors(n))) // n)
+
 
 def aperiodic_bound(w, k, sigma=2, sketch_size=1):
-    if (w+k)*np.log2(sigma) > 31:
+    if (w + k) * np.log2(sigma) > 31:
         return ragnar_ceil_LB(w, k)
-        
+
     sampled = 0
-    for d in divisors(w+k):
+    for d in divisors(w + k):
         primitive_n = aperiodic_necklaces(d, sigma)
-        slack = math.ceil(sketch_size*d / w)
+        slack = math.ceil(sketch_size * d / w)
         sampled += primitive_n * slack
-    return Fraction(sampled , (sigma ** (w+k)))
+    return Fraction(sampled, (sigma ** (w + k)))
+
 
 def aperiodic_bound_suff(w, k, sigma=2, sketch_size=1):
     ret = aperiodic_bound(w, k, sigma, sketch_size)
     if k % w != 1:
-        ret = max(ret, aperiodic_bound(w, math.ceil(k/w)*w + 1, sigma, sketch_size))
+        ret = max(ret, aperiodic_bound(w, math.ceil(k / w) * w + 1, sigma, sketch_size))
     return ret
-    
+
+
 def ragnar_ceil_LB(w, k):
-    return Fraction(math.ceil((w+k) / w) , (w+k))
+    return Fraction(math.ceil((w + k) / w), (w + k))
+
 
 def ragnar_WABI_LB(w, k):
     return 1.5 / (w + k - 0.5)
+
 
 def min_under_rot(s):
     mv = s
@@ -96,6 +109,7 @@ def min_under_rot(s):
             mv = s
     return mv
 
+
 def get_rots(s):
     rots = list()
     mv = s
@@ -104,13 +118,17 @@ def get_rots(s):
         s = s[1:] + s[0]
     return rots
 
+
 @cache
 def get_necklaces(v, sigma=2):
-    nodes = ("".join(x) for x in itertools.product(list(str(x) for x in range(sigma)), repeat=v))
+    nodes = (
+        "".join(x)
+        for x in itertools.product(list(str(x) for x in range(sigma)), repeat=v)
+    )
     lwords = set()
     for vmer in nodes:
         lwords.add(min(vmer := (vmer[-1] + vmer[:-1]) for _ in range(len(vmer))))
-    
+
     necklaces = defaultdict(list)
     for lw in lwords:
         word = lw
